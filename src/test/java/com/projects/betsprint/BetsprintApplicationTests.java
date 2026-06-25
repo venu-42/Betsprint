bf298bf39d8bfb7d31a130732fc7b67e30bd9b0e
venu-42 <solipuram42@gmail.com>
2026-06-25 20:18:05 +0530

    First commit

---
+++ src/test/java/com/projects/betsprint/BetsprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.betsprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
